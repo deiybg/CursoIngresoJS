cf0717e00cdd0cf5b982b186278ea202839bf014
deiybg <40776476+deiybg@users.noreply.github.com>
2018-07-05 21:57:32 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,16 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro ;
 
+	ancho = prompt("Ingrese el ancho de un ractangulo")
+	ancho = parseInt(ancho);
+	largo = prompt("Ingrese el largo de un ractangulo")
+	largo = parseInt(largo);
+
+	perimetro = ancho * largo;
+
+	alert("el perimetro del rectangulo es " + perimetro);
 }
