80ecff548bdb151ffb9afedaa0c53f38baa379e2
deiybg <40776476+deiybg@users.noreply.github.com>
2018-07-05 21:58:02 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,25 @@
 function mostrar()
 {
+	var precio;
+	var porcentaje;
+	var descuento;
+	var iva;
+	var elPrecioFinal;
+
+	precio = prompt("ingrese precio ");
+	porcentaje = prompt("ingrese porcentaje ");
+	porcentaje = parceInt(precio);
+	precio = parceInt(precio);
+
+	descuento = precio + porcentaje;
+	descuento = parceInt(descuento);
+
+	alert(descuento);
+
+
+
+
+	
+
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,20 @@
 
 function mostrar()
 {
+var base;
+var altura;
+var superficie;
+var perimetro;
+
+base = prompt("Ingrese la base ");
+base = parseInt(base);
+
+altura = prompt("Ingrese la altura ");
+altura = parseInt(base);
+
+superficie = base * altura;
+perimetro = (base + altura + base);
+
+alert("la superficie es " + superficie + " el perimetro es " + perimetro)
 
 }
