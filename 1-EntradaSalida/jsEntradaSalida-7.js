33dc5e6dcdb91f78b91162952302b60845bf3b39
deiybg <40776476+deiybg@users.noreply.github.com>
2018-07-12 19:58:20 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -1,24 +1,72 @@
-/*Debemos lograr tomar Los numeros por ID ,
+ /*Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = document.getElementById('numeroUno').value;
+	numeroUno = parseInt(numeroUno);
+
+	numeroDos = document.getElementById('numeroDos').value;
+	numeroDos = parseInt(numeroDos);
+
+	resultado = numeroUno + numeroDos;
+
+	alert("la suma es " + resultado);
 }
 
 function restar()
 {
-	
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = document.getElementById('numeroUno').value;
+	numeroUno = parseInt(numeroUno);
+
+	numeroDos = document.getElementById('numeroDos').value;
+	numeroDos = parseInt(numeroDos);
+
+	resultado = numeroUno - numeroDos;
+
+	alert("la resta es " + resultado);
 }
 
 function multiplicar()
 { 
-	
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = document.getElementById('numeroUno').value;
+	numeroUno = parseInt(numeroUno);
+
+	numeroDos = document.getElementById('numeroDos').value;
+	numeroDos = parseInt(numeroDos);
+
+	resultado = numeroUno * numeroDos;
+
+	alert("la multiplicacion es " + resultado);
 }
 
 function dividir()
 {
-	
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+
+	numeroUno = document.getElementById('numeroUno').value;
+	numeroUno = parseInt(numeroUno);
+
+	numeroDos = document.getElementById('numeroDos').value;
+	numeroDos = parseInt(numeroDos);
+
+	resultado = numeroUno / numeroDos;
+
+	alert("la division es " + resultado);
 }
 
