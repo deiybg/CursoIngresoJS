1f1c1be50cdeef1ad1b642075030edf7ce1459c2
deiybg <40776476+deiybg@users.noreply.github.com>
2018-07-23 22:19:53 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -3,8 +3,29 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
+
+	switch (mesDelAño)
+	{
+
+		case "Febrero":
+
+			alert("Este mes tiene 28 dias");
+			break;
+
+		case "Enero":
+		case "Marzo":	
+		case "Mayo":
+		case "Julio":	
+		case "Agosto":
+		case "Octubre":
+		case "Diciembre":
+			alert("Este mes tiene 31 dias");
+			break;	
+
+		default :	
+
+			alert("Este mes tiene 30 dias");		
+	}
 	
 
 
