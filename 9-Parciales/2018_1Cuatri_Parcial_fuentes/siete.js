516fd75bbf2b48dc4c82ca301a86e260d5def35a
deiybg <40776476+deiybg@users.noreply.github.com>
2018-07-30 21:10:03 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,42 @@
 function mostrar()
 {
+	var notas;
+	var sexo;
+	var contador = 0;
+	var promedio;
 
+	while(contador<5)
+
+	{
+
+	contador++;	
+	notas = prompt("Ingresar una nota: ");
+	notas = parseInt(notas);
+
+	while(notas<0 || notas>10)
+	{
+		alert("ingrese una nota valida entre 0 y 10");
+		notas = prompt("Ingresar una nota: ");
+		notas = parseInt(notas);
+		
+	}
+
+
+	sexo = prompt("Ingresar un sexo: ");
+
+	while(sexo !="f" && sexo !="m")
+		{
+			alert("Ingrese un sexo valid8 f o m ");
+			sexo = prompt("Ingresar un sexo: ");
+
+		}
+
+	promedio = notas / contador;
+
+
+
+
+	}
+
+	alert("el promedio de las notas totales es: " + promedio);
 }
