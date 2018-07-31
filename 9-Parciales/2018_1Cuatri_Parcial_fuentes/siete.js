5f02bad1135087578340021192c701e17ea86207
deiybg <40776476+deiybg@users.noreply.github.com>
2018-07-30 22:13:58 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,6 +1,124 @@
+/*
+jose m 25
+maria f 16
+jesus m 33
+fer f 81
+*/
+
 function mostrar()
 {
-	var notas;
+	var nombre;
+	var sexo;
+	var edad;
+	var contador =0;
+	var contadorMujeres = 0;
+	var contadorHombres = 0;
+	var contadorMayorEdad =0;
+	var contadorMenorEdad=0;
+	var maximo;
+	var minimo;
+
+
+	while(contador <4 )
+
+
+	{
+		contador++;
+
+		nombre = prompt("ingrese su nombre: ");
+
+		sexo = prompt("Ingrese su sexo");
+		while(sexo !="m"&& sexo!="f")
+		{
+		sexo = prompt("Ingrese su sexo");
+		}
+		edad = prompt("Ingrese su edad:");
+		edad = parseInt(edad);
+		while(isNaN(edad) || edad<0 || edad >100)
+		{
+		edad = prompt("Ingrese su edad:");
+		edad = parseInt(edad);
+		}
+
+		if (sexo =="f") 
+		{
+		contadorMujeres++;
+		}
+		else
+		{
+		contadorHombres++;
+		}
+
+		if (edad>18) 
+		{
+		contadorMayorEdad++;
+		} 
+		else 
+		{
+		contadorMenorEdad++;
+		}
+
+		if (edad==1)
+		 {
+		 	maximo =edad;
+		 	minimo=edad;
+		 }
+		 else
+		 {
+		 	if (edad>maximo) 
+		 	{
+		 		maximo=edad;
+		 	}
+		 	if (edad<minimo) 
+		 	{
+		 		minimo=edad;
+		 	}
+		 }
+
+	}
+
+	
+	
+
+	document.write("Cantidad de mujeres = " + contadorMujeres );
+	document.write("Cantidad de hombres = " + contadorHombres );
+	document.write("Cantidad de mayores de edad = " + contadorMayorEdad );
+	document.write("Cantidad de menores de edad = " + contadorMenorEdad );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+/*var notas;
 	var sexo;
 	var contador = 0;
 	var promedio;
@@ -38,5 +156,4 @@ function mostrar()
 
 	}
 
-	alert("el promedio de las notas totales es: " + promedio);
-}
+	alert("el promedio de las notas totales es: " + promedio);*/
